d18d207514cad1a2f698572a2c75ddc712a0bd02
Vibhu <vibhuedits@gmail.com>
2021-06-14 03:53:01 +0530

    updated secrets

--- Secrets/app.js
+++ Secrets/app.js
@@ -36,7 +36,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema=new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -94,11 +95,20 @@ app.get("/login",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
-    }
+    // if(req.isAuthenticated()){
+    //     res.render("secrets");
+    // }else{
+    //     res.redirect("/login");
+    // }
+    User.find({"secret":{$ne: null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets",{userWithSecrets: foundUsers});
+            }
+        }
+    });
 });
 
 app.get("/register",function(req,res){
@@ -174,6 +184,31 @@ app.post("/login",function(req,res){
             });
         }
     })
+});
+
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit",function(req,res){
+    const submittedSecret=req.body.secret;
+    User.findById(req.user.id,function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets"); 
+                });
+            }
+        }
+    });
+
 });
 
 app.listen(3000,function(){
